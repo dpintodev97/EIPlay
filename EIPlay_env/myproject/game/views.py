194ff69ce30bff8436f19517981f852778c72a7b
Mañanas <davidpintodev97@gmail.com>
2024-04-15 10:18:00 +0200

    actualizamos la url, primwero en settings, y luego asociando el url.py de la app con el url.py del proyecto para que se visualice bien la web

--- EIPlay_env/myproject/game/views.py
+++ EIPlay_env/myproject/game/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 #Necesitamos definir la vista de la app (game/views.py) que renderice (esto lo hace el navegador web) el archivo html
 # estático.
-#Este HTML es eltípico index.html, que en  un Django project está guardado en la carpeta de la app :
+#Este HTML es el típico index.html, que en  un Django project está guardado en la carpeta de la app :
 # game/tempaltes/index.html  :
 def home(request):
-    return render(request, 'index.html')
+    #return HttpResponse('holi')
+   return render(request, 'index.html')
 
--- EIPlay_env/myproject/myproject/settings.py
+++ EIPlay_env/myproject/myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os #NECESARIO PARA LEER O ESCRIBIR ARCHIVOS, MANIPULAR RUTAS DE ARCHIVOS, TRABAJAR CON VARIABLES DE ENTORNO, ENTRE OTRAS
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game' #AÑADIMOS EL NOMBRE DE MI APP WEB , game
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], #ESPECIFICO DE DÓNDE COGE LOS TEMPLATES, COMO EL index.html PARA QUE SE VISUALICE
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
